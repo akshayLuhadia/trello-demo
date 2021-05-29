8c3d119b06d4473d870ac48f215ba8a333e6a9fa
akshayLuhadia <akshayluhadia.nmims@gmail.com>
2021-05-29 22:59:58 +0530

    updated node engine, api export

--- www/src/api.js
+++ www/src/api.js
@@ -1,13 +1,21 @@
-exports.execute = async function (resource, method, headers = {}, body = null) {
+export const execute = async function (
+  resource,
+  method,
+  headers = {},
+  body = null
+) {
   try {
-    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/${resource}`, {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-        ...headers,
-      },
-      ...(body && { body: JSON.stringify(body) }),
-    });
+    const res = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/${resource}`,
+      {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+          ...headers,
+        },
+        ...(body && { body: JSON.stringify(body) }),
+      }
+    );
     const data = await res.json();
     return data;
   } catch (error) {
